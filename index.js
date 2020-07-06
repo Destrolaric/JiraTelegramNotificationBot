5366e8036cb27330453b5bebfba5c0236563f5f0
Salemar <fireshowel@gmail.com>
2020-07-06 12:51:31 +0300

    `1`

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ var express = require('express'),
     app = express(),
     port = 3000;
 
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
